efc5927877d87d415eeb2bf6f6902df1c93df321
Aaron Jheng <wentworth@outlook.com>
2026-04-29 16:22:00 +0800

    Increase default window size to 1200x800

--- Sources/RedisConsole/UI/ContentView.swift
+++ Sources/RedisConsole/UI/ContentView.swift
@@ -113,7 +113,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             .environmentObject(AppStore.shared)
 
         let window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 960, height: 640),
+            contentRect: NSRect(x: 0, y: 0, width: 1200, height: 800),
             styleMask: [.titled, .closable, .miniaturizable, .resizable],
             backing: .buffered,
             defer: false
